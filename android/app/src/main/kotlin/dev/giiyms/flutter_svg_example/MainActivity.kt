7b4da18add66a62fe23a33addf2b93d2d3ab9d79
Giiyms <giiyms@fishbowl.dev>
2021-08-19 18:16:05 +0100

    init

---
+++ android/app/src/main/kotlin/dev/giiyms/flutter_svg_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.giiyms.flutter_svg_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SvgExample(),
+    );
+  }
+}
+
+class SvgExample extends StatelessWidget {
+  final assetName = 'assets/undraw_financial_data_es63.svg';
+
+  const SvgExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 48.0),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  assetName,
+                  semanticsLabel: 'Dart Logo',
+                  // width: 400,
+                  // fit: BoxFit.scaleDown,
+                ),
+                const SizedBox(height: 16.0),
+                const SizedBox(
+                  // width: 400,
+                  child: LinearProgressIndicator(
+                    minHeight: 4,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
